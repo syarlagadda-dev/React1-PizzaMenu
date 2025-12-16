696f096fc09b1cecb072ff1397ec2cb4eafc7a36
Sushanth Yarlagadda <sushanth.yarlagadda2006@gmail.com>
2025-12-16 14:45:48 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,76 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Pizza({ x }) {
+  return (
+    <div>
+      <img src={pizzaData[x].photoName} alt={pizzaData[x].name} />
+      <h2>{pizzaData[x].name}</h2>
+      <p>{pizzaData[x].ingredients}</p>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Fast React Pizza Co.</h1>
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
+      <Pizza x={0} />
+      <Pizza x={1} />
+      <Pizza x={2} />
+      <Pizza x={3} />
+      <Pizza x={4} />
+      <Pizza x={5} />
+    </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour < closeHour;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <footer>
+        {isOpen
+          ? "We are open until 10:00 PM. Come visit us or order online."
+          : "We are closed. Come back later!"}
+      </footer>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,3 +80,48 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
